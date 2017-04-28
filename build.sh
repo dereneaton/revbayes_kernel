302e16c5082e5c3fbac4a7b8164bf58420e7f8a4
deren <deren.eaton@yale.edu>
2017-04-28 15:28:44 -0400

    conda recipe

--- build.sh
+++
@@ -1,2 +0,0 @@
-sudo python3 setup.py install
-python3 -m revbayes_kernel.install
---
+++ conda.recipe/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## install the kernel
+$PYTHON setup.py install
+$PYTHON -m revbayes_kernel.install
+
